b52e11446e7cfab76250265057a476cd2415f085
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-07-03 16:58:09 +0900

    Initial commit

---
+++ src/test/java/com/eunsunzzang/posting/PostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunsunzzang.posting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
