8e4dbd0dde874b38df7f3fe85c533ce6b61eb794
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-07-28 09:44:23 +0900

    feat: errorcode 추가

--- src/main/java/com/eunsunzzang/posting/error/errorcode/AuthErrorCode.java
+++ src/main/java/com/eunsunzzang/posting/error/errorcode/AuthErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
     EMAIL_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
-    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일 입니다."),
-    AUTH_CODE_ERROR(HttpStatus.BAD_REQUEST,"잘못된 인증 코드 입니다.");
+    EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 이메일 입니다."),
+    AUTH_CODE_ERROR(HttpStatus.BAD_REQUEST,"잘못된 인증 코드 입니다."),
+    PASSWORD_NOT_CORRECT(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원 입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
